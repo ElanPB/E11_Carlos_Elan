d3f79ee6da8fc2563f940260f2084930de92604d
ElanPB <elanparkbernstein@berkeley.edu>
2023-02-01 18:30:18 -0800

    Fixed formatting issues with tabs in while loop

--- test.py
+++ test.py
@@ -30,9 +30,9 @@ while i < 100:
     print("Pressure: %0.3f hPa" % info.get('Pressure'))
     print("Altitude = %0.2f meters" % info.get('Altitude'))
     
-	df = df.append(info, ignore_index = True)
-
-	time.sleep(2)
+    df = df.append(info, ignore_index = True)
+    
+    time.sleep(2)
     
     i += 1
 
