7b391b1917cb449bb3336640617a19ba4904d76f
ElanPB <elanparkbernstein@berkeley.edu>
2023-03-15 16:04:19 -0700

    Created pocket_geiger python file

---
+++ pocket_geiger.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+start_time = time.time()
+runtime = 300
+
+pin_num = 23
+
+GPIO.add_event_detect(pin_num, GPIO.FALLING)
+
+detected_times = []
+
+while (time.time() - start_time > runtime):
+    if GPIO.event_detected(pin_num):
+        curr_time = time.time()
+        detected_times.append(curr_time)
+        print("Count at time: {:}".format(curr_time)
+
+print("Over the span of 300 seconds, we measured {:} counts.".format(len(detected_times)))
