08543806bbeae7fca4bc547fc867cef10f8ec669
ElanPB <elanparkbernstein@berkeley.edu>
2023-03-15 16:45:49 -0700

    Added comments to pocket_geiger.py

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -17,17 +17,22 @@ count = 0
 last_run = 0
 
 while ((time.time() - start_time) < runtime_minutes * 60):
+    # Check to see if it is a new minute based on a differnce in time time of the last run through the loop and current time
     if ((time.time() - start_time)%60 < last_run):
         cpm.append(count)
         print("There were {:} counts in the last minute.".format(count))
         count = 0
+    
+    # Set time for the run of this loop
     last_run = (time.time() - start_time)%60
-    print(GPIO.event_detected(pin_num))
+    
+    # Check for an event
     if GPIO.event_detected(pin_num):
         count += 1
         time_stamps.append(time.time())
         print("Count at time: {:}".format(time_stamps[-1]))
         
+# Make sure we have actually recorded all the data, if it did not register the final minute ending, append leftover data
 if(len(cpm) != runtime_minutes):
     cpm.append(count)
     
