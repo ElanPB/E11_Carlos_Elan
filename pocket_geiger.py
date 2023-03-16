1ecc806c1b2ab2836e540a5a49815b7007fd315f
ElanPB <elanparkbernstein@berkeley.edu>
2023-03-15 17:08:39 -0700

    Compacted content of while loop

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -17,22 +17,21 @@ count = 0
 last_run = 0
 
 while ((time.time() - start_time) < runtime_minutes * 60):
-    # Check to see if it is a new minute based on a differnce in time time of the last run through the loop and current time
-    if ((time.time() - start_time)%60 < last_run):
-        cpm.append(count)
-        print("There were {:} counts in the last minute.".format(count))
-        count = 0
-    
+
     # Set time for the run of this loop
     last_run = (time.time() - start_time)%60
     
     # Check for an event
     time_out = int((60 - last_run)*1000)
     channel = GPIO.wait_for_edge(pin_num, GPIO.FALLING, timeout = time_out)
-    if not (channel is None):
-        count += 1
+    if (channel is None):
+        cpm.append(count)
+        print("There were {:} counts in the last minute.".format(count))
+        count = 0
+    else:
         time_stamps.append(time.time())
         print("Count at time: {:}".format(time_stamps[-1]))
+        count += 1
         
 # Make sure we have actually recorded all the data, if it did not register the final minute ending, append leftover data
 if(len(cpm) != runtime_minutes):
