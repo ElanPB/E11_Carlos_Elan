9f76214a2c1d03ff675b2c754b72a444ab4fc4cb
ElanPB <elanparkbernstein@berkeley.edu>
2023-03-22 15:22:17 -0700

    Got rid of Elan's shitty version of pocket geiger

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -2,40 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 start_time = time.time()
-runtime_minutes = 5
-
-pin_num = 6
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin_num, GPIO.IN)
-GPIO.add_event_detect(pin_num, GPIO.FALLING)
-
-
+runtime = 5 * 60 #s
 cpm = []
 time_stamps = []
 count = 0
-last_run = 0
 
-while ((time.time() - start_time) < runtime_minutes * 60):
+GPIO.setmode(GPIO.BCM)
+pin_num = 6
 
-    # Set time for the run of this loop
-    last_run = (time.time() - start_time)%60
-    
-    # Check for an event
-    time_out = int((60 - last_run))*1000
-    print(last_run, time_out)
-    channel = GPIO.wait_for_edge(pin_num, GPIO.FALLING, timeout = time_out)
-    if (channel is None):
-        cpm.append(count)
-        print("There were {:} counts in the last minute.".format(count))
-        count = 0
-    else:
-        time_stamps.append(time.time())
-        print("Count at time: {:}".format(time_stamps[-1]))
-        count += 1
-        
-# Make sure we have actually recorded all the data, if it did not register the final minute ending, append leftover data
-if(len(cpm) != runtime_minutes):
+def my_callback(pin_num):
+    global time_stamps
+    time_stamps.append(time.time())
+    global count
+    count += 1
+    print("Event detected at" + str(time.time()))
+
+GPIO.setup(pin_num, GPIO.IN)
+GPIO.add_event_detect(pin_num, GPIO.FALLING, callback = my_callback)
+
+while ((time.time() - start_time) < runtime):
+    time.sleep(60)
     cpm.append(count)
-    
-print("Over the span of {} minutes, we measured {:} counts.".format(runtime_minutes, len(time_stamps)))
+    count = 0
+
+print("Over the span of %d seconds, we measured %d counts." %(runtime, len(time_stamps)))
