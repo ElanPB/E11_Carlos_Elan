c54b96f5371c4f4b0a0fcb51fe61113034334ce4
Carlos Norena <ccnorena@berkeley.edu>
2023-03-22 15:06:03 -0700

    Defined time stamps in correct location

--- pocket_geiger_carlos.py
+++ pocket_geiger_carlos.py
@@ -3,11 +3,15 @@ import time
 
 start_time = time.time()
 runtime = 5 * 60 #s
+cpm = []
+time_stamps = []
+count = 0
 
 GPIO.setmode(GPIO.BCM)
 pin_num = 23
 
 def my_callback(pin_num):
+    global time_stamps
     time_stamps.append(time.time())
     global count
     count += 1
@@ -15,10 +19,6 @@ def my_callback(pin_num):
 
 GPIO.setup(pin_num, GPIO.IN)
 GPIO.add_event_detect(pin_num, GPIO.FALLING, callback = my_callback(pin_num))
-
-cpm = []
-time_stamps = []
-count = 0
 
 while ((time.time() - start_time) < runtime):
     time.sleep(60)
